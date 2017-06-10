70bee2b4389f7d3e84bcbd5366881f64e861c4ce
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-11 00:56:34 +0530

    VH for row_layout

---
+++ app/src/main/java/com/appbusters/robinkamboj/udacitysyllabuspart1/view/activities/utils/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities.utils;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View v, int position, boolean isLongClick);
+}
---
+++ app/src/main/java/com/appbusters/robinkamboj/udacitysyllabuspart1/view/activities/views/holders/OneVH.java
@@ -0,0 +1,34 @@
+package com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities.views.holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.appbusters.robinkamboj.udacitysyllabuspart1.R;
+import com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities.utils.ItemClickListener;
+
+import butterknife.BindView;
+
+public class OneVH extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    private ItemClickListener clickListener;
+
+    @BindView(R.id.heading)
+    TextView heading;
+    @BindView(R.id.description)
+    TextView description;
+
+    public OneVH(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+    }
+
+    public void setClickListener(ItemClickListener itemClickListener){
+        this.clickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        clickListener.onClick(view, getAdapterPosition(), false);
+    }
+}
