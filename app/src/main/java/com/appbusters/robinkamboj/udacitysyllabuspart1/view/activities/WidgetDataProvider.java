29865b964d8838b1cce12fdea8a3c76ceed10752
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-24 11:10:38 +0530

    widget inflating with db items

--- app/src/main/java/com/appbusters/robinkamboj/udacitysyllabuspart1/view/activities/WidgetDataProvider.java
+++ app/src/main/java/com/appbusters/robinkamboj/udacitysyllabuspart1/view/activities/WidgetDataProvider.java
@@ -2,10 +2,13 @@ package com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 import com.appbusters.robinkamboj.udacitysyllabuspart1.R;
+import com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities.controller.MyDBHelper;
+import com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities.model.Data;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +18,9 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
     List<String> mCollections = new ArrayList<>();
     List<String> mCollections2 = new ArrayList<>();
     private Context context;
+
+    List<Data> datas = new ArrayList<>();
+    MyDBHelper dbHelper;
 
     public WidgetDataProvider(Context context, Intent intent) {
         this.context = context;
@@ -37,14 +43,20 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
 
     @Override
     public int getCount() {
-        return mCollections.size();
+        return datas.size();
     }
 
     @Override
     public RemoteViews getViewAt(int position) {
         RemoteViews mView = new RemoteViews(context.getPackageName(), R.layout.row_widget);
-        mView.setTextViewText(R.id.word, mCollections.get(position));
-        mView.setTextViewText(R.id.meaning, mCollections2.get(position));
+        mView.setTextViewText(R.id.word, datas.get(position).getHeading());
+
+        Log.e("SIZER", " " + datas.get(position).getHeading());
+
+        mView.setTextViewText(R.id.meaning, datas.get(position).getDescription());
+
+        Log.e("SIZER", " " + datas.get(position).getDescription());
+
         return mView;
     }
 
@@ -69,29 +81,44 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
     }
 
     private void initData() {
-        mCollections.clear();
-        mCollections2.clear();
+//        mCollections.clear();
+//        mCollections2.clear();
+//
+//        mCollections.add("Abate");
+//        mCollections2.add("To Decrease/Reduce");
+//        mCollections.add("Abdicate");
+//        mCollections2.add("To Give Up a Position, Right or Power");
+//        mCollections.add("Aberrant");
+//        mCollections2.add("Deviating From What Is Normal");
+//        mCollections.add("Abeyance");
+//        mCollections2.add("Temporary Suppression or Suspension");
+//        mCollections.add("Abject");
+//        mCollections2.add("Miserable, Pitiful");
+//
+//        mCollections.add("Abate");
+//        mCollections2.add("To Decrease/Reduce");
+//        mCollections.add("Abdicate");
+//        mCollections2.add("To Give Up a Position, Right or Power");
+//        mCollections.add("Aberrant");
+//        mCollections2.add("Deviating From What Is Normal");
+//        mCollections.add("Abeyance");
+//        mCollections2.add("Temporary Suppression or Suspension");
+//        mCollections.add("Abject");
+//        mCollections2.add("Miserable, Pitiful");
 
-        mCollections.add("Abate");
-        mCollections2.add("To Decrease/Reduce");
-        mCollections.add("Abdicate");
-        mCollections2.add("To Give Up a Position, Right or Power");
-        mCollections.add("Aberrant");
-        mCollections2.add("Deviating From What Is Normal");
-        mCollections.add("Abeyance");
-        mCollections2.add("Temporary Suppression or Suspension");
-        mCollections.add("Abject");
-        mCollections2.add("Miserable, Pitiful");
+        dbHelper = new MyDBHelper(context);
+        datas = dbHelper.getAllData();
 
-        mCollections.add("Abate");
-        mCollections2.add("To Decrease/Reduce");
-        mCollections.add("Abdicate");
-        mCollections2.add("To Give Up a Position, Right or Power");
-        mCollections.add("Aberrant");
-        mCollections2.add("Deviating From What Is Normal");
-        mCollections.add("Abeyance");
-        mCollections2.add("Temporary Suppression or Suspension");
-        mCollections.add("Abject");
-        mCollections2.add("Miserable, Pitiful");
+        Log.e("SIZERRRR", " " + datas.size());
+
+        int position = 1;
+
+        while (position<datas.size()){
+
+            Log.e("SIZER", " " + datas.get(position).getHeading());
+            Log.e("SIZER", " " + datas.get(position).getDescription());
+
+            position++;
+        }
     }
 }
