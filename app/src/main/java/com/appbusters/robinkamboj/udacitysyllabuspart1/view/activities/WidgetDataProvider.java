7a92f8eb2264dfa7a63f8ebda16efc751e20c575
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-24 10:54:18 +0530

    widget FINALLY in working state!

---
+++ app/src/main/java/com/appbusters/robinkamboj/udacitysyllabuspart1/view/activities/WidgetDataProvider.java
@@ -0,0 +1,97 @@
+package com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.appbusters.robinkamboj.udacitysyllabuspart1.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory{
+
+    List<String> mCollections = new ArrayList<>();
+    List<String> mCollections2 = new ArrayList<>();
+    private Context context;
+
+    public WidgetDataProvider(Context context, Intent intent) {
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate() {
+        initData();
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        initData();
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return mCollections.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews mView = new RemoteViews(context.getPackageName(), R.layout.row_widget);
+        mView.setTextViewText(R.id.word, mCollections.get(position));
+        mView.setTextViewText(R.id.meaning, mCollections2.get(position));
+        return mView;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    private void initData() {
+        mCollections.clear();
+        mCollections2.clear();
+
+        mCollections.add("Abate");
+        mCollections2.add("To Decrease/Reduce");
+        mCollections.add("Abdicate");
+        mCollections2.add("To Give Up a Position, Right or Power");
+        mCollections.add("Aberrant");
+        mCollections2.add("Deviating From What Is Normal");
+        mCollections.add("Abeyance");
+        mCollections2.add("Temporary Suppression or Suspension");
+        mCollections.add("Abject");
+        mCollections2.add("Miserable, Pitiful");
+
+        mCollections.add("Abate");
+        mCollections2.add("To Decrease/Reduce");
+        mCollections.add("Abdicate");
+        mCollections2.add("To Give Up a Position, Right or Power");
+        mCollections.add("Aberrant");
+        mCollections2.add("Deviating From What Is Normal");
+        mCollections.add("Abeyance");
+        mCollections2.add("Temporary Suppression or Suspension");
+        mCollections.add("Abject");
+        mCollections2.add("Miserable, Pitiful");
+    }
+}
---
+++ app/src/main/java/com/appbusters/robinkamboj/udacitysyllabuspart1/view/activities/WidgetProvider.java
@@ -0,0 +1,53 @@
+package com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities;
+
+import android.annotation.SuppressLint;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import com.appbusters.robinkamboj.udacitysyllabuspart1.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int widgetId : appWidgetIds) {
+            RemoteViews mView = initViews(context, appWidgetManager, widgetId);
+            appWidgetManager.updateAppWidget(widgetId, mView);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    @SuppressLint("NewApi")
+    private RemoteViews initViews(Context context,
+                                  AppWidgetManager widgetManager, int widgetId) {
+
+        RemoteViews mView = new RemoteViews("com.appbusters.robinkamboj.udacitysyllabuspart1",
+                R.layout.widget_provider);
+
+        Intent intent = new Intent(context, WidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        mView.setRemoteAdapter(widgetId, R.id.widgetCollectionList, intent);
+
+        return mView;
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
