b77491d072043ea25859830336af42ab18a32983
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-28 18:01:47 +0530

    Rect Custom View: Passingand Using Custom Attributes

--- app/src/main/java/com/appbusters/robinkamboj/udacitysyllabuspart1/view/activities/views/customviews/CustomView.java
+++ app/src/main/java/com/appbusters/robinkamboj/udacitysyllabuspart1/view/activities/views/customviews/CustomView.java
@@ -1,20 +1,23 @@
 package com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities.views.customviews;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.View;
+
+import com.appbusters.robinkamboj.udacitysyllabuspart1.R;
 
 public class CustomView extends View{
 
-    private static final int SQUARE_SIZE = 200;
+    private static final int SQUARE_SIZE_DEF = 200;
     private Rect rectSquare;
     private Paint paintSquare;
+    private int mSquareColor, mSquareSize;
 
     public CustomView(Context context) {
         super(context);
@@ -43,11 +46,23 @@ public class CustomView extends View{
     private void init(@Nullable AttributeSet set){
         rectSquare = new Rect();
         paintSquare = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paintSquare.setColor(Color.GREEN);
+
+        if(set == null)
+            return;
+
+
+        TypedArray array = getContext().obtainStyledAttributes(set, R.styleable.CustomView);
+
+        mSquareColor = array.getColor(R.styleable.CustomView_square_color, Color.GREEN);
+        mSquareSize = array.getDimensionPixelSize(R.styleable.CustomView_square_size, SQUARE_SIZE_DEF);
+
+        paintSquare.setColor(mSquareColor);
+
+        array.recycle();
     }
 
     public void swapColor(){
-        paintSquare.setColor(paintSquare.getColor()==Color.GREEN ? Color.RED : Color.GREEN);
+        paintSquare.setColor(paintSquare.getColor()== mSquareColor ? Color.RED : mSquareColor);
 
         postInvalidate();
     }
@@ -58,8 +73,8 @@ public class CustomView extends View{
 
         rectSquare.left = 50;
         rectSquare.top = 50;
-        rectSquare.right = rectSquare.left + SQUARE_SIZE;
-        rectSquare.bottom = rectSquare.top + SQUARE_SIZE;
+        rectSquare.right = rectSquare.left + mSquareSize;
+        rectSquare.bottom = rectSquare.top + mSquareSize;
 
         canvas.drawRect(rectSquare, paintSquare);
     }
