274c0f8b2901aa24f601ac5165d46d60b6d4b780
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-29 14:40:54 +0530

    Calculator test for addn, subtn, division

---
+++ app/src/test/java/com/appbusters/robinkamboj/udacitysyllabuspart1/CalculatorTest.java
@@ -0,0 +1,27 @@
+package com.appbusters.robinkamboj.udacitysyllabuspart1;
+
+import com.appbusters.robinkamboj.udacitysyllabuspart1.view.activities.model.MyCalculatorModel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void checkAddition() throws Exception {
+        assertEquals(4, MyCalculatorModel.add(2, 2));
+    }
+
+    @Test
+    public void checkSubtraction() throws Exception {
+        assertEquals(4, MyCalculatorModel.subtract(6, 2));
+    }
+
+    @Test
+    public void checkDivision() throws Exception {
+        assertEquals("INFINITY", MyCalculatorModel.divide(4, 0));
+        assertEquals("NOT DEFINED", MyCalculatorModel.divide(0, 0));
+        assertEquals(2, Integer.parseInt(MyCalculatorModel.divide(4, 2)));
+    }
+}
